07f5ddfd579f69dfef9be6e62dd6c9382ed263c8
Shane Grimminck <shane.grimminck@gmail.com>
2013-03-15 18:24:43 -0400

    Added UI png's - Forced copy to output

--- Besieged/BesiegedClient/Rendering/RenderGameWindow.cs
+++ Besieged/BesiegedClient/Rendering/RenderGameWindow.cs
@@ -16,6 +16,20 @@ namespace BesiegedClient.Rendering
             canvas.Children.Clear();
             canvas.Background = RenderingUtilities.SeaGreenBrush;
 
+            double aspectRatio = Math.Round((double)dimensions.Width / (double)dimensions.Height, 2, MidpointRounding.AwayFromZero);
+
+            string UIComponentPath = string.Empty;
+
+            if (aspectRatio == 1.33)
+            {
+                //4:3
+                UIComponentPath = @"resources\UI\4x3\";
+            }
+            else
+            {
+                //16:9
+                UIComponentPath = @"resources\UI\16x9\";
+            }
             
         }
     }
---
+++ Besieged/BesiegedClient/Rendering/RenderMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BesiegedClient.Rendering
+{
+    public static class RenderMenu
+    {
+        private static Dimensions dimensions;
+        public static void RenderMainMenu(Canvas canvas)
+        {
+            dimensions = new Dimensions() { Width = (int)canvas.Width, Height = (int)canvas.Height };
+            canvas.Children.Clear();
+            canvas.Background = RenderingUtilities.SeaGreenBrush;
+        }
+        public static void RenderOptionsMenu(Canvas canvas)
+        {
+            dimensions = new Dimensions() { Width = (int)canvas.Width, Height = (int)canvas.Height };
+        }
+    }
+}
