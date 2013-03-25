5d25843e139af64ad575ed50f8e2d0f6380230d9
Jesse Carter <jesse.r.carter@gmail.com>
2013-03-25 14:41:02 -0400

    More render cleanup
    Modified the UML to include the new render class

--- Besieged/BesiegedClient/Engine/Dialog/RenderMessageDialog.cs
+++ Besieged/BesiegedClient/Engine/Dialog/RenderMessageDialog.cs
@@ -14,20 +14,20 @@ namespace BesiegedClient.Engine.Dialog
 {
     public static class RenderMessageDialog
     {
-        private static List<UIElement> DialogComponents;
-        private static Dimensions dimensions;
-        private static BitmapImage XImg = new BitmapImage(new Uri("resources\\UI\\CloseDialog.png", UriKind.RelativeOrAbsolute));
-        private static BitmapImage clickedXImg = new BitmapImage(new Uri("resources\\UI\\ClickedCloseDialog.png", UriKind.RelativeOrAbsolute));
+        private static List<UIElement> m_DialogComponents;
+        private static Dimensions m_Dimensions;
+        private static BitmapImage m_XImage = new BitmapImage(new Uri("resources\\UI\\CloseDialog.png", UriKind.RelativeOrAbsolute));
+        private static BitmapImage m_ClickedXImage = new BitmapImage(new Uri("resources\\UI\\ClickedCloseDialog.png", UriKind.RelativeOrAbsolute));
 
         public static void RenderMessage(string message)
         {
             ClearDialog();
-            DialogComponents = new List<UIElement>();
+            m_DialogComponents = new List<UIElement>();
             foreach (UIElement el in ClientGameEngine.Get().Canvas.Children)
             {
                 el.IsEnabled = false;
             }
-            dimensions = ClientGameEngine.Get().ClientDimensions;
+            m_Dimensions = ClientGameEngine.Get().ClientDimensions;
             string UIComponentPath = "resources\\UI\\";
             Image img;
             BitmapImage bimg = new BitmapImage();
@@ -36,13 +36,13 @@ namespace BesiegedClient.Engine.Dialog
             try
             {
                 Rectangle rect = new Rectangle(); //create the rectangle
-                rect.Width = dimensions.Width;
-                rect.Height = dimensions.Height;
+                rect.Width = m_Dimensions.Width;
+                rect.Height = m_Dimensions.Height;
                 rect.Opacity = 0.7;
                 Canvas.SetLeft(rect,0);
                 Canvas.SetTop(rect, 0);
                 rect.Fill = Utilities.Rendering.BlackBrush;
-                DialogComponents.Add(rect);
+                m_DialogComponents.Add(rect);
             }
             catch (Exception)
             {
@@ -56,10 +56,10 @@ namespace BesiegedClient.Engine.Dialog
                 img.Height = bimg.PixelHeight * 2;
                 signDimensions.Width= bimg.PixelWidth * 2;
                 signDimensions.Height = bimg.PixelHeight * 2;
-                Canvas.SetLeft(img, dimensions.Width / 2 - img.Width / 2);
-                Canvas.SetBottom(img, dimensions.Height / 2 - img.Height / 2); //this should center this on the screen... I hope...
+                Canvas.SetLeft(img, m_Dimensions.Width / 2 - img.Width / 2);
+                Canvas.SetBottom(img, m_Dimensions.Height / 2 - img.Height / 2); //this should center this on the screen... I hope...
                 Canvas.SetZIndex(img, 1100); //range 1100 - 1200 for error dialogs
-                DialogComponents.Add(img);
+                m_DialogComponents.Add(img);
             }
             catch (Exception)
             {
@@ -74,10 +74,10 @@ namespace BesiegedClient.Engine.Dialog
                 textLabel.TextWrapping = TextWrapping.Wrap;
                 textLabel.FontFamily = new FontFamily("Papyrus");
                 textLabel.FontSize = 16.0;
-                Canvas.SetLeft(textLabel, (dimensions.Width / 2) - (bimg.PixelWidth) + bimg.PixelWidth*0.10);
-                Canvas.SetBottom(textLabel, dimensions.Height / 2 - bimg.PixelHeight ); //this should center this on the screen... I hope...
+                Canvas.SetLeft(textLabel, (m_Dimensions.Width / 2) - (bimg.PixelWidth) + bimg.PixelWidth*0.10);
+                Canvas.SetBottom(textLabel, m_Dimensions.Height / 2 - bimg.PixelHeight ); //this should center this on the screen... I hope...
                 Canvas.SetZIndex(textLabel, 1110); //range 1100 - 1200 for error dialogs
-                DialogComponents.Add(textLabel);
+                m_DialogComponents.Add(textLabel);
             }
             catch (Exception)
             {
@@ -87,53 +87,53 @@ namespace BesiegedClient.Engine.Dialog
             try
             {
                 img = new Image();
-                img.Source = XImg;
-                img.Width = XImg.PixelWidth;
-                img.Height = XImg.PixelHeight;
-                Canvas.SetLeft(img, dimensions.Width / 2 + signDimensions.Width * 0.30);
-                Canvas.SetBottom(img, dimensions.Height / 2 +signDimensions.Height * 0.30); //this should center this on the screen... I hope...
+                img.Source = m_XImage;
+                img.Width = m_XImage.PixelWidth;
+                img.Height = m_XImage.PixelHeight;
+                Canvas.SetLeft(img, m_Dimensions.Width / 2 + signDimensions.Width * 0.30);
+                Canvas.SetBottom(img, m_Dimensions.Height / 2 +signDimensions.Height * 0.30); //this should center this on the screen... I hope...
                 Canvas.SetZIndex(img, 1200); //range 1100 - 1200 for error dialogs
 
                 img.MouseDown += (s, e) =>
                 {
                     Image scopedImg = s as Image;
-                    scopedImg.Source = clickedXImg;
+                    scopedImg.Source = m_ClickedXImage;
                 };
 
                 img.MouseUp += (s, e) =>
                     {
                         Image scopedImg = s as Image;
-                        scopedImg.Source = XImg;
+                        scopedImg.Source = m_XImage;
                         ClearDialog();
                     };
                 img.MouseLeave += (s, e) =>
                     {
                         Image scopedImg = s as Image;
-                        if (scopedImg.Source == clickedXImg)
-                            scopedImg.Source = XImg;
+                        if (scopedImg.Source == m_ClickedXImage)
+                            scopedImg.Source = m_XImage;
                     };
 
-                DialogComponents.Add(img);
+                m_DialogComponents.Add(img);
             }
             catch (Exception)
             {
             }
 
-            foreach (UIElement obj in DialogComponents)
+            foreach (UIElement obj in m_DialogComponents)
             {
                 ClientGameEngine.Get().Canvas.Children.Add(obj);
             }
         }
 
-        public static void RenderInput(string Display, EventHandler TextChangedHandler)
+        public static void RenderInput(string display, EventHandler textChangedHandler)
         {
             ClearDialog();
-            DialogComponents = new List<UIElement>();
+            m_DialogComponents = new List<UIElement>();
             foreach (UIElement el in ClientGameEngine.Get().Canvas.Children)
             {
                 el.IsEnabled = false;
             }
-            dimensions = ClientGameEngine.Get().ClientDimensions;
+            m_Dimensions = ClientGameEngine.Get().ClientDimensions;
             string UIComponentPath = "resources\\UI\\";
             Image img;
             BitmapImage bimg = new BitmapImage();
@@ -142,13 +142,13 @@ namespace BesiegedClient.Engine.Dialog
             try
             {
                 Rectangle rect = new Rectangle(); //create the rectangle
-                rect.Width = dimensions.Width;
-                rect.Height = dimensions.Height;
+                rect.Width = m_Dimensions.Width;
+                rect.Height = m_Dimensions.Height;
                 rect.Opacity = 0.7;
                 Canvas.SetLeft(rect, 0);
                 Canvas.SetTop(rect, 0);
                 rect.Fill = Utilities.Rendering.BlackBrush;
-                DialogComponents.Add(rect);
+                m_DialogComponents.Add(rect);
             }
             catch (Exception)
             {
@@ -162,10 +162,10 @@ namespace BesiegedClient.Engine.Dialog
                 img.Height = bimg.PixelHeight * 2;
                 signDimensions.Width = bimg.PixelWidth * 2;
                 signDimensions.Height = bimg.PixelHeight * 2;
-                Canvas.SetLeft(img, dimensions.Width / 2 - img.Width / 2);
-                Canvas.SetBottom(img, dimensions.Height / 2 - img.Height / 2); //this should center this on the screen... I hope...
+                Canvas.SetLeft(img, m_Dimensions.Width / 2 - img.Width / 2);
+                Canvas.SetBottom(img, m_Dimensions.Height / 2 - img.Height / 2); //this should center this on the screen... I hope...
                 Canvas.SetZIndex(img, 1100); //range 1100 - 1200 for error dialogs
-                DialogComponents.Add(img);
+                m_DialogComponents.Add(img);
             }
             catch (Exception)
             {
@@ -174,7 +174,7 @@ namespace BesiegedClient.Engine.Dialog
             {
                 //Label
                 TextBlock textLabel = new TextBlock();
-                textLabel.Text = Display;
+                textLabel.Text = display;
                 textLabel.Width = (bimg.PixelWidth * 2) * 0.9;
                 textLabel.Height = 50;
                 textLabel.TextWrapping = TextWrapping.NoWrap;
@@ -182,13 +182,13 @@ namespace BesiegedClient.Engine.Dialog
                 textLabel.FontSize = 20.0;
                 textLabel.MouseUp += (s, e) =>
                 {
-                    TextChangedHandler("The Input Text", new EventArgs());
+                    textChangedHandler("The Input Text", new EventArgs());
                     ClearDialog();
                 };
-                Canvas.SetLeft(textLabel, (dimensions.Width / 2) - (bimg.PixelWidth) + 10);
-                Canvas.SetTop(textLabel, dimensions.Height / 2 - bimg.PixelHeight * 0.85); 
+                Canvas.SetLeft(textLabel, (m_Dimensions.Width / 2) - (bimg.PixelWidth) + 10);
+                Canvas.SetTop(textLabel, m_Dimensions.Height / 2 - bimg.PixelHeight * 0.85); 
                 Canvas.SetZIndex(textLabel, 1110); //range 1100 - 1200 for error dialogs
-                DialogComponents.Add(textLabel);
+                m_DialogComponents.Add(textLabel);
             }
             catch (Exception)
             {
@@ -202,11 +202,11 @@ namespace BesiegedClient.Engine.Dialog
                 tbox.FontFamily = new FontFamily("Papyrus");
                 tbox.FontSize = 20.0;
                 tbox.Opacity = 0.6;
-                Canvas.SetLeft(tbox, (dimensions.Width / 2) - (bimg.PixelWidth) + 25);
-                Canvas.SetTop(tbox, dimensions.Height / 2 - tbox.Height + 15);
+                Canvas.SetLeft(tbox, (m_Dimensions.Width / 2) - (bimg.PixelWidth) + 25);
+                Canvas.SetTop(tbox, m_Dimensions.Height / 2 - tbox.Height + 15);
                 Canvas.SetZIndex(tbox, 1110); //range 1100 - 1200 for error dialogs
 
-                DialogComponents.Add(tbox);
+                m_DialogComponents.Add(tbox);
             }
             catch (Exception)
             {
@@ -231,13 +231,13 @@ namespace BesiegedClient.Engine.Dialog
                 };
                 textLabel.MouseUp += (s, e) =>
                 {
-                    TextChangedHandler(tbox.Text.Trim() != "" ? tbox.Text.Trim() : null, new EventArgs());
+                    textChangedHandler(tbox.Text.Trim() != "" ? tbox.Text.Trim() : null, new EventArgs());
                     ClearDialog();
                 }; 
-                Canvas.SetLeft(textLabel, (dimensions.Width / 2) - (bimg.PixelWidth * 0.25) );
-                Canvas.SetBottom(textLabel, dimensions.Height / 2 - bimg.PixelHeight); //this should center this on the screen... I hope...
+                Canvas.SetLeft(textLabel, (m_Dimensions.Width / 2) - (bimg.PixelWidth * 0.25) );
+                Canvas.SetBottom(textLabel, m_Dimensions.Height / 2 - bimg.PixelHeight); //this should center this on the screen... I hope...
                 Canvas.SetZIndex(textLabel, 1110); //range 1100 - 1200 for error dialogs
-                DialogComponents.Add(textLabel);
+                m_DialogComponents.Add(textLabel);
             }
             catch (Exception)
             {
@@ -262,19 +262,19 @@ namespace BesiegedClient.Engine.Dialog
                 };
                 textLabel.MouseUp += (s, e) =>
                 {
-                    TextChangedHandler(null, new EventArgs());
+                    textChangedHandler(null, new EventArgs());
                     ClearDialog();
                 }; 
-                Canvas.SetLeft(textLabel, (dimensions.Width / 2) + (bimg.PixelWidth * 0.25));
-                Canvas.SetBottom(textLabel, dimensions.Height / 2 - bimg.PixelHeight); //this should center this on the screen... I hope...
+                Canvas.SetLeft(textLabel, (m_Dimensions.Width / 2) + (bimg.PixelWidth * 0.25));
+                Canvas.SetBottom(textLabel, m_Dimensions.Height / 2 - bimg.PixelHeight); //this should center this on the screen... I hope...
                 Canvas.SetZIndex(textLabel, 1110); //range 1100 - 1200 for error dialogs
-                DialogComponents.Add(textLabel);
+                m_DialogComponents.Add(textLabel);
             }
             catch (Exception)
             {
             }
 
-            foreach (UIElement obj in DialogComponents)
+            foreach (UIElement obj in m_DialogComponents)
             {
                 ClientGameEngine.Get().Canvas.Children.Add(obj);
             }
@@ -282,8 +282,8 @@ namespace BesiegedClient.Engine.Dialog
 
         public static void ClearDialog()
         {
-            if (DialogComponents == null) return;
-            foreach (UIElement obj in DialogComponents)
+            if (m_DialogComponents == null) return;
+            foreach (UIElement obj in m_DialogComponents)
             {
                 if (ClientGameEngine.Get().Canvas.Children.Contains(obj))
                 {
@@ -294,7 +294,7 @@ namespace BesiegedClient.Engine.Dialog
             {
                 el.IsEnabled = true;
             }
-            DialogComponents.Clear();
+            m_DialogComponents.Clear();
         }
     }
 }
