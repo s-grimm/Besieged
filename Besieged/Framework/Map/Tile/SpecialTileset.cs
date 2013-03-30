9b717a38e9e1021ba3594603f4937fa33f04923e
Shane Grimminck <shane.grimminck@gmail.com>
2013-03-30 01:32:30 -0400

    Border Tile

---
+++ Besieged/Framework/Map/Tile/SpecialTileset.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Map.Tile
+{
+    public class Border : BaseTile
+    {
+        public override int CurrentHealthPointsModifier { get; set; }
+
+        public override int MaxHealthPointsModifier { get; set; }
+
+        public override int InitiativeModifier { get; set; }
+
+        public override int SkillModifier { get; set; }
+
+        public override int RangeModifier { get; set; }
+
+        public override int MovementModifier { get; set; }
+
+        public override bool IsPassable { get; set; }
+    }
+}
