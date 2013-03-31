92d1ab6d5626762c27f1e05ffeb7d3dacf0fe839
Shane Grimminck <shane.grimminck@gmail.com>
2013-03-30 22:25:14 -0400

    Implemented Start of InGameEngine

--- Besieged/BesiegedClient/Engine/State/InGameEngine/InGameEngine.cs
+++ Besieged/BesiegedClient/Engine/State/InGameEngine/InGameEngine.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using BesiegedClient.Engine.State.InGameEngine.State;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace BesiegedClient.Engine.State.InGameEngine
 {
-    class InGameEngine
+    public class InGameEngine
     {
+        IInGameState m_CurrentGameState;
+        IInGameState m_PreviousGameState;
+
+        public static InGameEngine m_Instance = null;
+
+        public Canvas GameCanvas { get; private set; }
+
+        private InGameEngine() { }
+
+        public static InGameEngine Get()
+        {
+            if (m_Instance == null)
+            {
+                m_Instance = new InGameEngine();
+            }
+            return m_Instance;
+        }
     }
 }
