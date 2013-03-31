d8433392b0f450b57a59d8ddb13ebcfdf3305926
Shane Grimminck <shane.grimminck@gmail.com>
2013-03-30 22:18:48 -0400

    InGameEngine and IInGameState

---
+++ Besieged/BesiegedClient/Engine/State/PlayingGameState.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BesiegedClient.Engine.State
+{
+    public class PlayingGameState : IGameState
+    {
+        private static PlayingGameState m_Instance = null;
+
+        private PlayingGameState() { }
+
+        public static IGameState Get()
+        {
+            try
+            {
+                if (m_Instance == null)
+                {
+                    m_Instance = new PlayingGameState();
+                    m_Instance.Initialize();
+                }
+                return m_Instance;
+            }
+            catch (Exception ex)
+            {
+                // error handling
+                throw ex;
+            }
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Render()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
