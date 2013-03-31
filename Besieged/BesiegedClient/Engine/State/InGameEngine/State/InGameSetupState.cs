0a9764d157310fa8cf774944f29fc4d963fe4ae5
Shane Grimminck <shane.grimminck@gmail.com>
2013-03-31 14:16:17 -0500

    Game Setup State

---
+++ Besieged/BesiegedClient/Engine/State/InGameEngine/State/InGameSetupState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BesiegedClient.Engine.State.InGameEngine.State
+{
+    public class InGameSetupState : IInGameState
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Render()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
