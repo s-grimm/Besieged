8922573582614bdaecad6f95382830a39e2fc371
Shane Grimminck <shane.grimminck@gmail.com>
2013-03-30 22:18:48 -0400

    InGameEngine and IInGameState

---
+++ Besieged/BesiegedClient/Engine/State/InGameEngine/State/IInGameState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BesiegedClient.Engine.State.InGameEngine.State
+{
+    public interface IInGameState
+    {
+            void Initialize();
+            void Render();
+            void Dispose();
+    }
+}
