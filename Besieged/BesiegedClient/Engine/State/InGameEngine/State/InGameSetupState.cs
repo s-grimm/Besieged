87218c7ec88d6331893ea9951afc7310bc60b9b7
Shane Grimminck <shane.grimminck@gmail.com>
2013-03-31 15:35:09 -0500

    Draw Colored Tiles on board

--- Besieged/BesiegedClient/Engine/State/InGameEngine/State/InGameSetupState.cs
+++ Besieged/BesiegedClient/Engine/State/InGameEngine/State/InGameSetupState.cs
@@ -1,29 +1,73 @@
-﻿using System;
+﻿using Framework.Map;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace BesiegedClient.Engine.State.InGameEngine.State
 {
     public class InGameSetupState : IInGameState
     {
+        private Color CreateRandomColor()
+        {
+            Random randonGen = new Random();
+            Color randomColor = Color.FromRgb((byte)randonGen.Next(0, 255), (byte)randonGen.Next(0, 255), (byte)randonGen.Next(0, 255));
+            return randomColor;
+        }
+
         private static InGameSetupState m_Instance = null;
-        private InGameSetupState() { }
+
+        private InGameSetupState()
+        {
+        }
+
+        private double _tileWidth = 50;
+        private double _tileHeight = 50;
+        private List<Rectangle> _rectangles = new List<Rectangle>();
 
         public void Initialize()
         {
             //throw new NotImplementedException();
+            GameMap map = InGameEngine.Get().GameBoard;
+
+            for (int i = 0; i < map.MapLength; i += 1)
+            {
+                for (int y = 0; y < map.MapHeight; y += 1)
+                {
+                    Rectangle rect = new Rectangle(); //create the rectangle
+                    rect.StrokeThickness = 1;  //border to 1 stroke thick
+                    rect.Stroke = new SolidColorBrush(Colors.Black); //border color to black
+                    rect.Fill = new SolidColorBrush(CreateRandomColor());
+                    rect.Width = 50;
+                    rect.Height = 50;
+                    rect.Name = "box" + i.ToString();
+                    Canvas.SetLeft(rect, i * _tileWidth);
+                    Canvas.SetTop(rect, y * _tileHeight);
+                    _rectangles.Add(rect);
+                }
+            }
         }
 
         public void Render()
         {
-            //throw new NotImplementedException();
+            Canvas target = InGameEngine.Get().GameCanvas;
+            foreach (Rectangle r in _rectangles)
+            {
+                target.Children.Add(r);
+            }
         }
 
         public void Dispose()
         {
-           // throw new NotImplementedException();
+            Canvas target = InGameEngine.Get().GameCanvas;
+            foreach (Rectangle r in _rectangles)
+            {
+                if (target.Children.Contains(r))
+                {
+                    target.Children.Remove(r);
+                }
+            }
         }
 
         public static IInGameState Get()
