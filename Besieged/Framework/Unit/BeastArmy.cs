e2f913c2fa18d955f8a1bb0f6ba4c30109e28e1e
Shane Grimminck <shane.grimminck@gmail.com>
2013-04-02 19:35:53 -0400

    BeastArmy no longer throws SOE

--- Besieged/Framework/Unit/BeastArmy.cs
+++ Besieged/Framework/Unit/BeastArmy.cs
@@ -61,43 +61,36 @@ namespace Framework.Unit
         public int MaxHealthPoints
         {
             get { return 6; }
-            set { MaxHealthPoints = value; }
         }
 
         public int Initiative
         {
             get { return 3; }
-            set { Initiative = value; }
         }
 
         public int Skill
         {
             get { return 3; }
-            set { Skill = value; }
         }
 
         public int Range
         {
             get { return 1; }
-            set { Range = value; }
         }
 
         public int Cost
         {
             get { return 5; }
-            set { Cost = value; }
         }
 
         public int Movement
         {
             get { return 6; }
-            set { Movement = value; }
         }
 
         public string Description
         {
             get { return "Gnoll Marauder"; }
-            set { Description = value; }
         }
     }
 
@@ -114,43 +107,36 @@ namespace Framework.Unit
         public int MaxHealthPoints
         {
             get { return 3; }
-            set { MaxHealthPoints = value; }
         }
 
         public int Initiative
         {
             get { return 2; }
-            set { Initiative = value; }
         }
 
         public int Skill
         {
             get { return 2; }
-            set { Skill = value; }
         }
 
         public int Range
         {
             get { return 3; }
-            set { Range = value; }
         }
 
         public int Cost
         {
             get { return 2; }
-            set { Cost = value; }
         }
 
         public int Movement
         {
             get { return 4; }
-            set { Movement = value; }
         }
 
         public string Description
         {
             get { return "Lizardman"; }
-            set { Description = value; }
         }
     }
 
@@ -167,43 +153,36 @@ namespace Framework.Unit
         public int MaxHealthPoints
         {
             get { return 5; }
-            set { MaxHealthPoints = value; }
         }
 
         public int Initiative
         {
             get { return 2; }
-            set { Initiative = value; }
         }
 
         public int Skill
         {
             get { return 3; }
-            set { Skill = value; }
         }
 
         public int Range
         {
             get { return 5; }
-            set { Range = value; }
         }
 
         public int Cost
         {
             get { return 5; }
-            set { Cost = value; }
         }
 
         public int Movement
         {
             get { return 4; }
-            set { Movement = value; }
         }
 
         public string Description
         {
             get { return "Lizardman Captain"; }
-            set { Description = value; }
         }
     }
 
@@ -220,43 +199,36 @@ namespace Framework.Unit
         public int MaxHealthPoints
         {
             get { return 5; }
-            set { MaxHealthPoints = value; }
         }
 
         public int Initiative
         {
             get { return 3; }
-            set { Initiative = value; }
         }
 
         public int Skill
         {
             get { return 3; }
-            set { Skill = value; }
         }
 
         public int Range
         {
             get { return 1; }
-            set { Range = value; }
         }
 
         public int Cost
         {
             get { return 5; }
-            set { Cost = value; }
         }
 
         public int Movement
         {
             get { return 8; }
-            set { Movement = value; }
         }
 
         public string Description
         {
             get { return "Taurus"; }
-            set { Description = value; }
         }
     }
 
@@ -273,43 +245,36 @@ namespace Framework.Unit
         public int MaxHealthPoints
         {
             get { return 8; }
-            set { MaxHealthPoints = value; }
         }
 
         public int Initiative
         {
             get { return 4; }
-            set { Initiative = value; }
         }
 
         public int Skill
         {
             get { return 4; }
-            set { Skill = value; }
         }
 
         public int Range
         {
             get { return 1; }
-            set { Range = value; }
         }
 
         public int Cost
         {
             get { return 10; }
-            set { Cost = value; }
         }
 
         public int Movement
         {
             get { return 8; }
-            set { Movement = value; }
         }
 
         public string Description
         {
             get { return "Taurus Lord"; }
-            set { Description = value; }
         }
     }
 
@@ -326,43 +291,36 @@ namespace Framework.Unit
         public int MaxHealthPoints
         {
             get { return 3; }
-            set { MaxHealthPoints = value; }
         }
 
         public int Initiative
         {
             get { return 2; }
-            set { Initiative = value; }
         }
 
         public int Skill
         {
             get { return 2; }
-            set { Skill = value; }
         }
 
         public int Range
         {
             get { return 3; }
-            set { Range = value; }
         }
 
         public int Cost
         {
             get { return 5; }
-            set { Cost = value; }
         }
 
         public int Movement
         {
             get { return 4; }
-            set { Movement = value; }
         }
 
         public string Description
         {
             get { return "Genie"; }
-            set { Description = value; }
         }
     }
 
@@ -379,43 +337,36 @@ namespace Framework.Unit
         public int MaxHealthPoints
         {
             get { return 6; }
-            set { MaxHealthPoints = value; }
         }
 
         public int Initiative
         {
             get { return 3; }
-            set { Initiative = value; }
         }
 
         public int Skill
         {
             get { return 3; }
-            set { Skill = value; }
         }
 
         public int Range
         {
             get { return 5; }
-            set { Range = value; }
         }
 
         public int Cost
         {
             get { return 8; }
-            set { Cost = value; }
         }
 
         public int Movement
         {
             get { return 4; }
-            set { Movement = value; }
         }
 
         public string Description
         {
             get { return "Master Genie"; }
-            set { Description = value; }
         }
     }
 
