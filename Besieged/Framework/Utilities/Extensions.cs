72547caa63c6edf80fa9a84ce26042007e804875
Shane Grimminck <shane.grimminck@gmail.com>
2013-04-02 21:19:39 -0400

    Revert "Revert "Basics to game state""
    This reverts commit 36f9ad83a409d13c680394e92f409448e6f7d704.

--- Besieged/Framework/Utilities/Extensions.cs
+++ Besieged/Framework/Utilities/Extensions.cs
@@ -26,5 +26,19 @@ namespace Framework.Utilities
                 list[n] = value;
             }
         }
+
+        public static void ArrayShuffle<T>(this T[] array)
+        {
+            Random random = new Random();
+            for (int i = array.Length; i > 1; i--)
+            {
+                // Pick random element to swap.
+                int j = random.Next(i); // 0 <= j <= i-1
+                // Swap.
+                T tmp = array[j];
+                array[j] = array[i - 1];
+                array[i - 1] = tmp;
+            }
+        }
     }
 }
