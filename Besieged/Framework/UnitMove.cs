e0ee8fe9b0ffb5248e177777eadef969d1839ff1
Shane Grimminck <shane.grimminck@gmail.com>
2013-04-13 18:54:00 -0400

    unit attack class

--- Besieged/Framework/UnitMove.cs
+++ Besieged/Framework/UnitMove.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Framework
+﻿namespace Framework
 {
     public class UnitMove
     {
         public Coordinate StartCoordinate { get; set; }
+
         public Coordinate EndCoordinate { get; set; }
 
-        public UnitMove() { }
+        public UnitMove()
+        {
+        }
+
         public UnitMove(Coordinate start, Coordinate end)
         {
             StartCoordinate = start;
@@ -19,12 +17,33 @@ namespace Framework
         }
     }
 
+    public class UnitAttack
+    {
+        public Coordinate Attacker { get; set; }
+
+        public Coordinate Defender { get; set; }
+
+        public UnitAttack()
+        {
+        }
+
+        public UnitAttack(Coordinate attacker, Coordinate defender)
+        {
+            Attacker = attacker;
+            Defender = defender;
+        }
+    }
+
     public class Coordinate
     {
         public int XCoordinate { get; set; }
+
         public int YCoordinate { get; set; }
 
-        public Coordinate() { }
+        public Coordinate()
+        {
+        }
+
         public Coordinate(int x, int y)
         {
             XCoordinate = x;
